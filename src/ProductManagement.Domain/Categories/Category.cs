e1e8e19017306338cb16440bd51c31cd25fa400a
Fatma-Eraslan <fatoseraslan275@gmail.com>
2022-09-07 14:40:53 +0300

    entities and consts added, DbContext revised

---
+++ src/ProductManagement.Domain/Categories/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace ProductManagement.Categories
+{
+    public class Category : AuditedAggregateRoot<Guid> //Burada, Guid varlığın birincil anahtar (Kimlik) türüdür. 
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/ProductManagement.Domain/Data/ProductManagementDataSeedContributor.cs
@@ -0,0 +1,32 @@
+﻿using ProductManagement.Categories;
+using ProductManagement.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+
+namespace ProductManagement.Data
+{
+    public class ProductManagementDataSeedContributor : IDataSeedContributor, ITransientDependency
+    {
+        private readonly IRepository<Category,Guid> _categoryRepository;
+        private readonly IRepository<Product,Guid> _productRepository;
+
+        public ProductManagementDataSeedContributor(
+        IRepository<Category, Guid> categoryRepository,
+        IRepository<Product, Guid> productRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _productRepository = productRepository;
+        }
+
+        public async Task SeedAsync(DataSeedContext context)
+        {
+            /***** TODO: Seed initial data here *****/
+        }
+    }
+}
