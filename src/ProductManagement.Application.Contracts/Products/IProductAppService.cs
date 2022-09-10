0c8aaa6fb23136f883ce06f4005bc7d0fc4e7704
Fatma-Eraslan <fatoseraslan275@gmail.com>
2022-09-10 18:40:30 +0300

    DTOs, Productappservice

---
+++ src/ProductManagement.Application.Contracts/Products/IProductAppService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace ProductManagement.Products
+{
+    public interface IProductAppService:IApplicationService
+    {
+        Task<PagedResultDto<ProductDto>> GetListAsync(PagedAndSortedResultRequestDto input);
+        /* 
+         • IProductAppService,  IApplicationService arabiriminden türetilmiştir.  Bu sayede ABP, uygulama hizmetlerini tanıyabilir. 
+         • GetListAsync yöntemi, ABP Framework'ün standart DTO sınıfı olan PagedAndSortedResultRequestDto'yu alır.
+    MaxResultCount (int), SkipCount (int) ve Sorting (string) özellikleri. 
+         • GetListAsync yöntemi, TotalCount  (long) özelliğini ve  ProductDto  nesnelerinin Items koleksiyonunu  içeren  PagedResultDto<ProductDto> döndürür  . Bu, ABP Framework ile sayfalanmış sonuçları döndürmenin uygun bir yoludur. 
+         */
+    }
+}
