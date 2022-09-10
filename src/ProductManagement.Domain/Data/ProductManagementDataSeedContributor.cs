d70a2515a9436cb5717badd217a011341e5d8718
Fatma-Eraslan <fatoseraslan275@gmail.com>
2022-09-10 17:54:31 +0300

    seed data added

--- src/ProductManagement.Domain/Data/ProductManagementDataSeedContributor.cs
+++ src/ProductManagement.Domain/Data/ProductManagementDataSeedContributor.cs
@@ -27,6 +27,42 @@ namespace ProductManagement.Data
         public async Task SeedAsync(DataSeedContext context)
         {
             /***** TODO: Seed initial data here *****/
+            if (await _categoryRepository.CountAsync() > 0)
+            {
+                return; // her çalıştırmada aynı verileri eklememizi önlemek için. 
+            }
+            var monitors = new Category { Name = "Monitors" };
+            var printers = new Category { Name = "Printers" };
+
+            await _categoryRepository.InsertManyAsync(new[] { monitors, printers });
+           
+            var monitor1 = new Product
+            {
+                Category = monitors,
+                Name = "XP VH240a 23.8-Inch Full HD 1080p IPS LED Monitor",
+                Price = 163,
+                ReleaseDate = new DateTime(2019, 05, 24),
+                StockState = ProductStockState.InStock
+            };
+            var monitor2 = new Product
+            {
+                Category = monitors,
+                Name = "Clips 328E1CA 32-Inch Curved Monitor, 4K UHD",
+                Price = 349,
+                IsFreeCargo = true,
+                ReleaseDate = new DateTime(2022, 02, 01),
+                StockState = ProductStockState.PreOrder
+            };
+            var printer1 = new Product
+            {
+                Category = monitors,
+                Name = "Acme Monochrome Laser Printer, Compact All-In One",
+                Price = 199,
+                ReleaseDate = new DateTime(2020, 11, 16),
+                StockState = ProductStockState.NotAvailable
+            };
+
+            await _productRepository.InsertManyAsync(new[] { monitor1, monitor2, printer1 });
         }
     }
 }
